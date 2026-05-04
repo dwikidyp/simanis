a78c5f0fdc433a4b83a7f8496e1933b1c57fa871
dwikidyp <dwiki834@gmail.com>
2026-05-04 10:00:18 +0700

    Push simanis

---
+++ src/bootstrap/providers.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\AkademikPanelProvider::class,
+    App\Providers\Filament\GuruPanelProvider::class,
+    App\Providers\Filament\OrangTuaPanelProvider::class,
+    App\Providers\Filament\SiswaPanelProvider::class,
+];

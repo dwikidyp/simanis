1e25167bab9db70c297d455d8c21470245d1599f
dwikidyp <dwiki834@gmail.com>
2026-05-11 10:03:25 +0700

    Cihuyy

---
+++ src/app/Filament/Admin/Resources/AkademikStaffResource/Pages/CreateAkademikStaff.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\AkademikStaffResource\Pages;
+
+use App\Filament\Admin\Resources\AkademikStaffResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateAkademikStaff extends CreateRecord
+{
+    protected static string $resource = AkademikStaffResource::class;
+}
